665a674a9ff907225e88f3b341ddb249faed1989
Your Name <vinishasv97@gmail.com>
2024-11-03 06:30:21 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/prediction_web_app/',
+  base: '/prediction_webapp/',
 })
